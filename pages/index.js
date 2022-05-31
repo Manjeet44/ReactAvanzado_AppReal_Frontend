d78cad1e064230c30381bd571e9b958bb6019576
Manjeet44 <andrew_vab@hotmail.com>
2022-05-31 20:21:57 +0200

    v5

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout'
 import {useQuery, gql}  from '@apollo/client';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const OBTENER_CLIENTES_USUARIO = gql`
   query obtenerClientesVendedor{
@@ -32,6 +33,10 @@ const Index = () => {
     <div>
       <Layout>
         <h1 className='text-2xl text-gray-800 font-light'>Clientes</h1>
+        <Link href="/nuevocliente">
+          <a className='bg-blue-800 py-2 px-5 mt-5 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold'>Nuevo Cliente</a>
+        </Link>
+
 
         <table className='table-auto shadow-md mt-10 w-full w-lg'>
           <thead className='bg-gray-800'>
@@ -46,7 +51,7 @@ const Index = () => {
               <tr key={cliente.id}>
                 <td className='border px-4 py-2'>{cliente.nombre} {cliente.apellido}</td>
                 <td className='border px-4 py-2'>{cliente.empresa}</td>
-                <td className='border px-4 py-2'>{cliente.email} {cliente.apellido}</td>
+                <td className='border px-4 py-2'>{cliente.email}</td>
               </tr>
             ))}
           </tbody>
