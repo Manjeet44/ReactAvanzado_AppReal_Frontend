6017415f54226d12046606a2749e345ce807fecc
Manjeet44 <andrew_vab@hotmail.com>
2022-06-07 21:08:28 +0200

    preboda

--- pages/nuevopedido.js
+++ pages/nuevopedido.js
@@ -1,21 +1,81 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import Layout from '../components/Layout';
 import AsignarCliente from '../components/pedidos/AsignarCliente';
 import AsignarProductos from '../components/pedidos/AsignarProductos';
 import ResumenPedido from '../components/pedidos/ResumenPedido';
 import Total from '../components/pedidos/Total';
+import { useMutation, gql } from '@apollo/client';
+import { useRouter } from 'next/router';
+import Swal from 'sweetalert2';
 
 import PedidoContext from '../context/pedidos/PedidoContext';
 
+const NUEVO_PEDIDO = gql`
+  mutation nuevoPedido($input: PedidoInput) {
+    nuevoPedido(input: $input) {
+      id
+    }
+  }
+`;
+
 const NuevoPedido = () => {
+  const router = useRouter();
+
+  const [mensaje, setMensaje] = useState(null);
+
   //Utilizar context y extrare sus funciones y valores
   const pedidoContext = useContext(PedidoContext);
+  const {cliente, productos, total} = pedidoContext;
   
+
+  //Mutation para crear un nuevo pedido
+  const [nuevoPedido] = useMutation(NUEVO_PEDIDO);
   
+  const validarPedido = () => {
+    return !productos.every( producto => producto.cantidad > 0 ) || total === 0 || cliente.length === 0 ? " opacity-50 cursor-not-allowed " : "";
+
+  }
+  
+  const crearNuevoPedido = async () => {
+    const {id} = cliente;
+    //Remover lo no deseadod e productos
+    const pedido = productos.map(({__typename, existencia, ...producto}) => producto)
+    try {
+      const {data} = await nuevoPedido({
+        variables: {
+          input: {
+            cliente: id,
+            total,
+            pedido
+          }  
+        }
+      });
+      router.push('/pedidos');
+      Swal.fire(
+        'Correcto',
+        'El pedido se registro correctamente',
+        'success'
+      )
+    } catch (error) {
+      setMensaje(error.message.replace('GraphQL error: ', ''));
+      setTimeout(() => {
+        setMensaje(null)
+      }, 3000)
+    }
+  }
+
+  const mostrarMensaje = () => {
+    return (
+      <div className='bg-red-200 border-solid border-2 border-gray-500 py-2 px-3 w-full my-3 max-w-sm text-center mx-auto'>
+        <p>{mensaje}</p>
+      </div>
+    )
+  }
     
   return (
         <Layout>
             <h1 className='text-2xl font-light text-gray-800'>Crear Nuevo Pedido</h1>
+            {mensaje && mostrarMensaje()}
             <div className='flex justify-center mt-5'>
               <div className='w-full max-w-lg'>
                 <AsignarCliente/>
@@ -24,7 +84,8 @@ const NuevoPedido = () => {
                 <Total/>
                 <button
                   type='button'
-                  className={`bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900`}
+                  className={`bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 ${validarPedido()}`}
+                  onClick={() => crearNuevoPedido()}
                 >Registrar Pedido</button>
               </div>
             </div>
--- pages/pedidos.js
+++ pages/pedidos.js
@@ -1,7 +1,32 @@
 import Layout from '../components/Layout';
 import Link from 'next/link';
+import { gql, useQuery } from '@apollo/client';
+import Pedido from '../components/Pedido';
+
+const OBTENER_PEDIDOS = gql`
+  query obtenerPedidosVendedor{
+    obtenerPedidosVendedor{
+      id
+      pedido{
+        id
+        cantidad
+        nombre
+      }
+      cliente
+      vendedor
+      total
+      estado
+    }
+  }
+`;
 
 const Pedidos = () => {
+  const {data, loading, error} = useQuery(OBTENER_PEDIDOS);
+
+  if(loading) return 'Cargando...';
+
+  const { obtenerPedidosVendedor } = data;
+
   return (
     <div>
       <Layout>
@@ -9,6 +34,14 @@ const Pedidos = () => {
         <Link href='/nuevopedido'>
           <a className='bg-blue-800 py-2 px-5 mt-5 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold'>Nuevo Pedido</a>
         </Link>
+        {obtenerPedidosVendedor.length === 0 ? (
+          <p className='mt-5 text-center text-2xl'>No hay pedidos aun</p>
+        ) : (obtenerPedidosVendedor.map(pedido => (
+          <Pedido
+            key={pedido.id}
+            pedido={pedido}
+          />
+        )))}
       </Layout>
     </div>
   )
