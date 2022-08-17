85a28677312bf456585c8b90bb61fd5d18df4d8b
Manjeet44 <andrew_vab@hotmail.com>
2022-08-17 20:09:26 +0200

    build

--- config/apollo.js
+++ config/apollo.js
@@ -2,7 +2,7 @@ import {ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:4000/',
+    uri: 'https://crmredux.herokuapp.com/',
     fetch: fetch
 });
 
